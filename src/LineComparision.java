fde11018d02a53e4f991c798076384cbf5bdc485
Mohd Anwar Shaikh <mohdanwarshaikh9@gmail.com>
2024-06-18 17:36:11 +0530

    UC2-Check_If_Lineis_equal file added

--- src/LineComparision.java
+++ src/LineComparision.java
@@ -1,15 +1,23 @@
 
 public class LineComparision {
-
-    public void isEqual(double x1,double x2,double y1,double y2){
+    public double isEqual(double x1,double x2,double y1,double y2){
         double length_line = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
-        System.out.println("Length of line is :"+length_line);
+        return length_line;
     }
-
     public static void main(String[] args) {
 
-        LineComparision uc1 = new LineComparision();
-        uc1.isEqual(1, 5, 4, 9);
+        LineComparision uc2 = new LineComparision();
+        double ans1_length_line=uc2.isEqual(2, 4, 5, 6);
+        double ans2_length_line=uc2.isEqual(2, 4, 5, 6);
+
+        if(ans1_length_line == ans2_length_line){
+            System.out.println("Length of  both lines are equal");
+        }
+        else{
+            System.out.println("Length of  both lines are not equal");
+
+        }
+
 
 
     }
